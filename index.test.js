284d6eb7b1a1cbf7671c6a2b982715b78f6be113
gfy100 <Fengyuan.Gong@gmail.com>
2019-10-15 12:10:09 -0400

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -4,7 +4,7 @@ test('that weekNum returns a value', () => {
   expect(weekNum).not.toBeNull();
 });
 
-// new test added
+// new test added test new
 test('that weekNum returns a number', () => {
   expect(weekNum).toBeDefined();
 });
